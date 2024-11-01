9fe170fc08c7fc3cf6278f3b6c2068ade43fdf12
Vinícius Micuanski <130366100+Micuanski@users.noreply.github.com>
2024-11-01 15:11:12 +0000

    AJUSTES MENU ROTAS E CSS

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const app = express();
 
 const PORT = 3303;
+app.use(express.static(__dirname + "/public"));
 
 app.get("/", function (req, res) {
   res.sendFile(__dirname + "/src/views/index.html");
 });
 app.get("/sobre", function (req, res) {
   res.sendFile(__dirname + "/src/views/about.html");
+});
+
+app.get("/contato", function (req, res) {
+  res.sendFile(__dirname + "/src/views/contato.html");
 });
 
 // ROTA UTILIZANDO MIDDLEWARE (404)
